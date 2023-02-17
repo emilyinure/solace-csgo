166e060c84a04fe2e6e0012781a78e08795008b9
emilyinure <emily.meow111@gmail.com>
2023-02-17 14:05:37 -0500

    Support for integer sliders

--- solace-csgo/controls/slider.hh
+++ solace-csgo/controls/slider.hh
@@ -5,14 +5,19 @@
 #include <fstream>
 
 class c_slider : public c_base_control {
-	float* value_;
+    int* ivalue_;
+	float* fvalue_;
 	float minimum_{ 0.f }, maximum_{ 100.f };
 	bool dragging_{ false };
 	area_t button_area_{ 0,0,0,0 };
 public:
-	c_slider( const char* name, float* value, float minimum = 0.f, float maximum = 100.f ) {
+    c_slider( const char* name, void* value, float minimum = 0.f, float maximum = 100.f, bool int_display = false )
+    {
 		this->name = name;
-		this->value_ = value;
+        if (int_display)
+            this->ivalue_ = static_cast<int*>(value);
+        else
+		    this->fvalue_ = static_cast<float*>(value);
 		this->minimum_ = minimum;
 		this->maximum_ = maximum;
 		this->type = control_type_slider;
@@ -24,21 +29,29 @@ public:
 	
 	auto draw( ) -> void override {
 		button_area_ = { this->area.x, this->area.y + ( this->area.h / 2 ) + ( this->area.h / 2 ) / 4.f, this->area.w, ( ( this->area.h / 2 ) / 2.f ) };
-		const auto slider_fill{ ( ( *this->value_ - this->minimum_ ) / ( this->maximum_ - this->minimum_ ) * ( button_area_.w-2.f ) ) };
-		
-		auto text_height = g.m_render->get_text_height( this->name, g.m_render->m_constantia_12( ) );
+        float temp_value = 0;
+        if (this->ivalue_)
+            temp_value = static_cast<float>(*this->ivalue_);
+        else if (this->fvalue_)
+            temp_value = *this->fvalue_;
+		const auto slider_fill{ ( ( temp_value - this->minimum_ ) / ( this->maximum_ - this->minimum_ ) * ( button_area_.w-2.f ) ) };
+
+        const auto text_height = render_t::get_text_height( this->name, g.m_render->m_constantia_12( ) );
 
 		g.m_render->filled_rect( button_area_.x + 1.f, button_area_.y + 1.f, slider_fill, button_area_.h - 2.f, menu.main_theme );
 		g.m_render->outlined_rect( button_area_.x, button_area_.y, button_area_.w, button_area_.h, { 240,240,240, 14 } );
 
-		g.m_render->text( g.m_render->m_constantia_12( ), this->area.x, this->area.y + this->area.h/4 - text_height/2, menu.bright, this->name );
+        render_t::text( g.m_render->m_constantia_12( ), this->area.x, this->area.y + this->area.h/4 - text_height/2, menu.bright, this->name );
 
 		// display.
-		char display[ 32 ];
-		sprintf_s( display, "%.1f / %.1f", *this->value_, this->maximum_ );
+        char display[32];
+        if (this->ivalue_)
+            sprintf_s(display, "%d / %d", *this->ivalue_, int(round(this->maximum_)) );
+        else if (this->fvalue_)
+		    sprintf_s( display, "%.1f / %.1f", *this->fvalue_, this->maximum_ );
 		const auto text_size = g.m_render->get_text_width( display, g.m_render->m_constantia_12( ) );
 
-		g.m_render->text( g.m_render->m_constantia_12( ), button_area_.x + button_area_.w - ( text_size ), this->area.y + this->area.h/4 - text_height / 2, menu.main_theme, display );
+        render_t::text( g.m_render->m_constantia_12( ), button_area_.x + button_area_.w - static_cast<float>(text_size), this->area.y + this->area.h/4 - static_cast<float>(text_height) / 2, menu.main_theme, display );
 	}
 
 	auto update( ) -> void override {
@@ -59,16 +72,34 @@ public:
 
 		menu.focused_control = this;
 
-		*this->value_ = this->minimum_ + ( this->maximum_ - this->minimum_ ) * ( input_helper.m_mouse_position( ).x - ( button_area_.x ) ) / ( button_area_.w );
-		*this->value_ = std::clamp( *this->value_, this->minimum_, this->maximum_ );
+        if (this->ivalue_) {
+            *this->ivalue_ = static_cast<int>(roundf(this->minimum_ + (this->maximum_ - this->minimum_) *
+                                                     (input_helper.m_mouse_position().x - (button_area_.x)) /
+                                                     (button_area_.w)));
+            *this->ivalue_ = std::clamp(*this->ivalue_, int(roundf(this->minimum_)), int(roundf(this->maximum_)));
+        }
+        else if (this->fvalue_)
+        {
+            *this->fvalue_ = this->minimum_ + (this->maximum_ - this->minimum_) *
+                                                 (input_helper.m_mouse_position().x - (button_area_.x)) /
+                                                 (button_area_.w);
+            *this->fvalue_ = std::clamp(*this->fvalue_, this->minimum_, this->maximum_);
+        }
 	}
 
-	void save() override {
-		std::cout << *this->value_ << "\n";
+	void save() override
+    {
+        if (this->ivalue_)
+            std::cout << *this->ivalue_ << "\n";
+        else if (this->fvalue_)
+            std::cout << *this->fvalue_ << "\n";
 	}
 	void load() override {
 		std::string line;
-		std::getline(std::cin, line);
-		std::istringstream(line) >> *this->value_;
+        std::getline(std::cin, line);
+        if (this->ivalue_)
+            std::istringstream(line) >> *this->ivalue_;
+        else if (this->fvalue_)
+            std::istringstream(line) >> *this->fvalue_;
 	}
 };
